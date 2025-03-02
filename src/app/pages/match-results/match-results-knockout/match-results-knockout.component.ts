ac42b81cf5e2dc7ac629977f5b5a9b5530dfa928
Pieter-Bas IJdens <pieter-bas@ijdens.com>
2025-03-02 17:50:51 +0100

    Version 1.1.18 Bugfix - synchronizing tab-changes

--- src/app/pages/match-results/match-results-knockout/match-results-knockout.component.ts
+++ src/app/pages/match-results/match-results-knockout/match-results-knockout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { KeysPipe } from '../../../pipes/keys.pipe';
 import { CommonModule } from '@angular/common';
 import { MatchResultModel } from '../../../models/match-result-model';
@@ -13,7 +13,7 @@ import { ApiService } from '../../../services/api.service';
   templateUrl: './match-results-knockout.component.html',
   styleUrl: './match-results-knockout.component.less'
 })
-export class MatchResultsKnockoutComponent implements OnInit {
+export class MatchResultsKnockoutComponent implements OnInit, OnChanges {
   tab = 0;
 
   @Input() match?: MatchModel;
@@ -21,13 +21,24 @@ export class MatchResultsKnockoutComponent implements OnInit {
   @Input() refreshTimeout: number = 1;
   @Input() refreshTimeoutCache: number = 1;
 
-  titels = ['', 'Achtste finale', 'Kwart finale', 'Halve finale', 'Finale'];  
+  titels = ['', 'Achtste finale', 'Kwartfinale', 'Halve finale', 'Finale'];  
 
   constructor(public apiService: ApiService) {    
   }
 
-  async ngOnInit(): Promise<void> {
+  async ngOnChanges(changes: SimpleChanges): Promise<void> {
+    console.log('changes', changes);
+    if (changes['results']) {
+      await this.refresh();
+    }
+  }
+
+  async refresh(): Promise<void> {
     this.tab = +(await this.apiService.getMatchUiSetting(this.match?.id?? -1, "ActiveResultsFinalsTab") ?? "0");
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.refresh();
   }
 
   async updateActiveTab(tabId: number) {
